8b978b6d09c20f148afa762a2365990ba5aa1be0
mostakim8 <allmostakim13@gmail.com>
2026-03-14 07:31:10 +0600

    error solved

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -21,7 +21,7 @@ const itemVariants: Variants = {
   visible: {
     opacity: 1,
     y: 0,
-    transition: { duration: 0.8, ease: [0.22, 1, 0.36, 1] as any },
+    transition: { duration: 0.8, ease: [0.22, 1, 0.36, 1] as const },
   },
 };
 
